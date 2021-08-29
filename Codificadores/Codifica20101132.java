8a4fbed1cea7e7465156a465f85141f70227c886
loudornelles <75826968+loudornelles@users.noreply.github.com>
2021-08-29 19:44:24 -0300

    Create Codifica20101132.java

---
+++ Codificadores/Codifica20101132.java
@@ -0,0 +1,47 @@
+package Codificadores;
+
+public class Codifica20101132 implements Codifica {
+    private static String tabela = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz@#";
+    private static int deslc = 7;
+
+    @Override
+    public String codifica(String str) {
+        String codificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                codificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        codificada += tabela.charAt(i + deslc);
+                    }
+                }
+            }
+        }
+        return codificada;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        String decodificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                decodificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        decodificada += tabela.charAt(i - deslc);
+                    }
+                }
+            }
+        }
+        return decodificada;
+    }
+
+	
+    String getNomeAutor();
+		return "Louise Dornelles";
+
+    String getMatriculaAutor();
+		return "20101132";	
+}
