4d85b525069a68f87613655cad10af1425996c00
RicardoFGuimaraes <ricf.guima@gmail.com>
2022-03-16 02:04:00 -0300

    FInalizado

--- App.java
+++ App.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 import Codificadores.Codifica;
+import Codificadores.Codifica2010;
 
 public class App {
     public static void executaCodificacao(Codifica cod, String frase) {
--- Codificadores/Codifica2010.java
+++ Codificadores/Codifica2010.java
@@ -4,48 +4,51 @@ public class Codifica2010 implements Codifica {
     private static String tabela = "XYZWVUTSRQPOMNOLQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz@#";
     private static int deslc = 2;
 
+    public Codifica2010() {
+    }
+
     @Override
     public String codifica(String str) {
-        String codificada = "";
-        for (char s : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(s)) {
-                codificada += s;
-            } else {
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s) {
-                        codificada += tabela.charAt(i + deslc);
-                    }
-                }
-            }
+        StringBuilder codificada = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            var s = Character.isLetterOrDigit(c) ? encodeChar(c) : c;
+            codificada.append(s);
         }
-        return codificada;
+
+        return codificada.toString();
     }
 
     @Override
     public String decodifica(String str) {
-        String decodificada = "";
-        for (char s : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(s)) {
-                decodificada += s;
-            } else {
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s) {
-                        decodificada += tabela.charAt(i - deslc);
-                    }
-                }
-            }
+        StringBuilder decodificada = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            var s = Character.isLetterOrDigit(c) ? decodeChar(c) : c;
+            decodificada.append(s);
         }
-        return decodificada;
+
+        return decodificada.toString();
     }
 
     @Override
     public String getMatriculaAutor() {
-        return "201024882";
+        return "202806816";
     }
 
     @Override
     public String getNomeAutor() {
-        return "Arthur Luz (calopsita)";
+        return "Ricardo Ferreira Guimarães";
+    }
+
+    private char encodeChar(char c) {
+        return tabela.charAt((tabela.indexOf(c) + deslc) % tabela.length());
+    }
+
+    private char decodeChar(char c) {
+        var x = tabela.indexOf(c) - deslc;
+
+        return tabela.charAt(x > 0 ? x : tabela.length() - deslc);
     }
 
 }
