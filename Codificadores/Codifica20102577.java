681f338d76394a9e5be80cb6d3ca10ca57c3db24
Bruno Avellar <br.avellar01@gmail.com>
2022-03-18 16:26:14 -0300

    Update Codifica20102577.java

--- Codificadores/Codifica20102577.java
+++ Codificadores/Codifica20102577.java
@@ -1,37 +1,53 @@
 package Codificadores;
 
+//Mudei a tabela utilizada e o valor de desloc para 4
+
 public class Codifica20102577 implements Codifica {
+    private static String tabela = "zyxwvutsrqponmlkjihgfedcba1234567890@#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static int deslc = 4;
 
     @Override
-    public String codifica(String str){
-       int codificada = Integer.parseInt(str);
-        String codPront = "";
-
-       codificada = codificada * 11;
-        codPront = Integer.toString(codificada);
-
-        return codPront;
-
+    public String codifica(String str) {
+        String codificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                codificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        codificada += tabela.charAt(i + deslc);
+                    }
+                }
+            }
+        }
+        return codificada;
     }
 
     @Override
-    public String decodifica(String str){
-        int decodificada = Integer.parseInt(str);
-         String decodPront = "";
-
-            decodificada = decodificada/11;
-            decodPront = Integer.toString(decodificada);
-
-        return decodPront;
+    public String decodifica(String str) {
+        String decodificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                decodificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        decodificada += tabela.charAt(i - deslc);
+                    }
+                }
+            }
+        }
+        return decodificada;
     }
 
     @Override
     public String getMatriculaAutor() {
         return "20102577";
     }
+
     @Override
     public String getNomeAutor() {
-        return "Bruno S A Avellar";
+        return "Bruno Avellar";
     }
 
 }
