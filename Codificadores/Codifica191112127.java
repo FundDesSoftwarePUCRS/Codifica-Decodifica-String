85cc1ca5a5725d6360112bc2d6b5d00523b40c5e
Daniele Paz Machado Gomes <Daniele.Paz@edu.pucrs.br>
2022-08-15 18:26:30 -0300

    :sparkles: Adicionar codificador 191112127

---
+++ Codificadores/Codifica191112127.java
@@ -0,0 +1,29 @@
+package Codificadores;
+
+public class Codifica191112127 implements Codifica {
+
+    @Override
+    public String getNomeAutor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String codifica(String str) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
