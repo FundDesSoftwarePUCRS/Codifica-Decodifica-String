4a14af0614cf51dcc6be6d05118ecf5088c0bed0
Leo_da_cruz <leonardo.cruz@edu.pucrs.br>
2022-08-15 15:32:05 -0300

    Classe criada,faltando o codificador e decodificador

---
+++ Codificadores/Codifica202040242.java
@@ -0,0 +1,27 @@
+package Codificadores;
+
+public class Codifica202040242 implements Codifica {
+
+    @Override
+    public String codifica(String str) {
+        //fazer um codificador
+        return null;
+    }
+
+    @Override
+    public String decodifica(String str) {
+        //fazer um decodificador
+        return null;
+    }
+
+    @Override
+    public String getNomeAutor() {
+       
+        return "Leonardo Cruz";
+    }
+
+    @Override
+    public String getMatriculaAutor() {
+        return "20204024-2";
+    }
+}
