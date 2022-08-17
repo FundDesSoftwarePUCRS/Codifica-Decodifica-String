f51c70ef16663126124dbb0ba28e8e4d07accc47
ETropea <e.tropea@edu.pucrs.br>
2022-08-17 01:33:19 +0000

    :sparkles:

--- Codificadores/Codifica211004296.java
+++ Codificadores/Codifica211004296.java
@@ -4,12 +4,12 @@ public class Codifica211004296 implements Codifica {
 
     @Override
     public String codifica(String str) {
-        return "Cachorro longo" + str + "destaque em programação orientada a dados";
+        return "cachorro longo" + str + "programacao orientada a dados";
     }
 
     @Override
     public String decodifica(String str) {
-        return str.substring(2, str.length() - 6);
+        return str.substring(14, str.length() - 29);
     }
 
     @Override
