28b98ba50d153db02b8f0e19e75e9bc3448122bb
Rafael.Schaker <71292308+Rafael-Schaker@users.noreply.github.com>
2022-08-29 01:13:01 -0300

    atualização requisitada

--- Codificadores/Codifica201073152.java
+++ Codificadores/Codifica201073152.java
@@ -1,30 +1,24 @@
 package Codificadores;
 
-public class Codifica201073152 implements Codifica {
+public class CodificaAnderson implements Codifica {
+
+    @Override
+    public String getNomeAutor() {
+        return "Rafael Schaker";
+    }
 
     @Override
     public String codifica(String str) {
-        String strnovo="";
-        for(int i=0;i<str.length();i++)
-        {   strnovo += 0;
-            strnovo += str.charAt(i);}
-        return strnovo;
+        return "@_@" + str + "@_@";
     }
 
     @Override
     public String decodifica(String str) {
-        String strnovo="";
-        for(int i=1; i<str.length() ;i+=2){strnovo += str.charAt(i);}
-        return strnovo;
+        return str.substring(3, str.length() - 3);
     }
 
     @Override
     public String getMatriculaAutor() {
         return "20107315-2";
-    }
-
-    @Override
-    public String getNomeAutor() {
-        return "Rafael Schaker";
     }
 }
